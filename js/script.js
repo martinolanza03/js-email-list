02ada8b94362f6ce65d14322d3f2bedfdba83cfe
martinolanza03 <lanzamartino3@gmail.com>
2025-03-13 15:36:09 +0100

    feat: create a fuction for create a <li> in the html, cicle for and axios get

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
 const linkApi = 'https://flynn.boolean.careers/exercises/api/random/mail';
-const ulElement = document.querySelector('.list-group');
+let ulElement = document.querySelector('.list-group');
 
+for (let i = 0; i < 10; i++) {
 
-axios.get(linkApi)
-    .then(response => {
-        const result = response.data;
+    axios.get(linkApi)
+        .then(response => {
+            const result = response.data;
+            createLi(result.response);
 
-        console.log(result.response);
-    })
+            console.log(result.response);
+        })
+        .catch(error => {
+            error = 'ERRORE!!!!!'
+            createLi(error);
+        });
+}
+
 
 
 
 /*
-    Creare nell'html
+    Funzione creare i li nell'html
  <li class="list-group-item"></li>
 
 */
+
+function createLi(email) {
+    let liElement = document.createElement('li');
+    liElement.classList.add('list-group-item');
+    liElement.append(email);
+    ulElement.append(liElement);
+
+
+    console.log(liElement);
+
+    return ulElement;
+
+}
